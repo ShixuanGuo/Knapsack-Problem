6662600c335127dd3f44512479b96ddd83f09259
Shixuan Guo <shixuanguo97@gmail.com>
2020-07-10 10:32:28 -0500

    Update Generate_report.py

--- Generate_report.py
+++ Generate_report.py
@@ -81,9 +81,3 @@ def write_opt(filename,N,n):
         wr.write('\n')    
     wr.close()
     return filename
-
-# Example
-treasures=read_treasure('Knapsack_Instances.csv')
-opt_c=mento_calor_opt(treasures[0:500], 50)
-result01 = final_subsets(treasures[0:500],opt_c)
-write_opt("Knapsack_solution_report.csv",500,50)
