de64a8c5cc14401b2b2f2c436b898343d75b486b
Shixuan Guo <shixuanguo97@gmail.com>
2020-07-10 10:10:28 -0500

    Update Dynamic programming(1).py

--- Dynamic programming(1).py
+++ Dynamic programming(1).py
@@ -6,34 +6,27 @@ C=100
 Choose which items to put in the knapsack 
 If you go over C, then you get nothing for that instance'''
 
-#Part 1: Get the information
-
-#Read the file
-def read_treasure(file_name):
-    '0:weight; 1:value'
-    f=open(file_name, 'r')
-    lines=f.readlines()
-    treasures=[[lines[i].strip('\n'),lines[i+1].strip('\n')] \
-              for i in range(0,1500,3)]
-    return treasures
-
-#Get the weights and values of all items
-def get_treasure(treasure):
-    weights=treasure[0].split(',')
-    values=treasure[1].split(',')
-    for i in range(len(weights)):
-        weights[i]=int(weights[i]) 
-        values[i]=float(values[i]) 
-    return weights, values
-
-
 # Part 2: Dynamic programming
 '''Find the collection of items whoes total weight is no more than the limited knapsack size C
 and the total value is the highest'''
 
 def bag(weights,values,C,n):
-    '''Find the total value we can get, 
-    given the weights and values of all items, the number of items and the limited size (C) 
+    '''Find the total value we can get given a list of items
+    
+    Parameters
+    ----------
+    weights: list 
+        the weights of all items
+    values:  list
+        the values of all items
+    C: int
+        the limited capacity
+    n: int
+        the number of items  
+    Return
+    ------
+    matrix
+        all possible item combinations with total weights satisfied capacity limitation
     '''
     bag=np.zeros((n+1,C+1),dtype=np.float32) 
     for i in range(1, n + 1):
@@ -44,8 +37,23 @@ def bag(weights,values,C,n):
     return bag
     
  def best_subset(weights, bag, C, n):
-    '''Find the combination of items with the highest value,
-    return a list of 0/1 
+    '''Find the combination of items with the highest value
+    
+    Parameters
+    ----------
+    weights: list 
+        the weights of all items
+    bag:  matrix 
+        all possible item combinations with total weights satisfied capacity limitation
+    C: int
+        the limited capacity
+    n: int
+        the number of items  
+    
+    Return 
+    ------
+    list
+        a list of 0/1 which is the best subset of items we select
     '''
     x = [0 for i in range(n)]
     j = C
